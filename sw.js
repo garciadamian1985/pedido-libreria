ceb2f82e435843af2b1e2654b699ac8c304cc8d6
garciadamian1985 <garciadamian1985@gmail.com>
2026-08-29 14:13:28 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE="pedido-libreria-v1";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
